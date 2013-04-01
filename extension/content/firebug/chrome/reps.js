370ea219863aa3080d12878f63b54e4c40974e42
Florent FAYOLLE <florent.fayolle69@gmail.com>
2013-04-01 17:27:59 +0200

    issue6291_master: fix failure for issue 3645

--- extension/content/firebug/chrome/reps.js
+++ extension/content/firebug/chrome/reps.js
@@ -2361,8 +2361,9 @@ FirebugReps.StackTrace = domplate(Firebug.Rep,
             var frame = frames[i];
             var sf = frame.sourceFile;
             if ((sf && sf.href && Str.hasPrefix(sf.href, "chrome")) ||
-                (frame.fn == "_firebugRerun") ||
-                (frame.fn == "jsdbug_NoScriptFunctionName"))
+                (frame.fn === "_firebugRerun") ||
+                (frame.fn === "jsdbug_NoScriptFunctionName") ||
+                (frame.fn === "(debugger)"))
             {
                 continue;
             }
