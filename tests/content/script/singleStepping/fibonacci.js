d916e8238e0ec27c3065aa67cab98f2d33a98714
Florent FAYOLLE <florent.fayolle69@gmail.com>
2014-02-25 21:56:24 +0100

    Issue 6789: Change singleStepping FBTest for this feature (needs to be backed out)

--- tests/content/script/singleStepping/fibonacci.js
+++ tests/content/script/singleStepping/fibonacci.js
@@ -3,3 +3,11 @@ function fibonacci(n) {
     return n < 0 ? 0 : n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+(function()
+{
+    // xxxFlorent: From one of the two lines above. The debugger would resume.
+    window.__defineGetter__('a', () => console.log("get a"));
+    a + a;
+})();
+
